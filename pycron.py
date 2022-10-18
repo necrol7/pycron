86dc24b225d6f24e9ceab7c132abe8554d89aa09
necrol7 <era.necrol@gmail.com>
2022-10-18 23:51:41 +0300

    third commit

--- pycron.py
+++ pycron.py
@@ -1,13 +1,20 @@
 import configparser
 import os
 
-def readConfig(path):   # Читаем конфиг файл
+app_settings = {}
+
+
+def readConfig(path,app_settings):
     config = configparser.ConfigParser()
     config.read(path)
-    log-level = config.get("Settings", "log-level")
-    log-file = config.get("Settings", "log-file")
+    app_settings["log_file"] = config.get("Settings", "log_file")
+    app_settings["log_level"] = config.get("Settings", "log_level")
+    print(app_settings["log_level"])
+    print(app_settings["log_file"])
     
- 
 if __name__ == "__main__":
     path = "pycron.conf"
-    readConfig(path)
+    readConfig(path,app_settings)
+    print(app_settings["log_level"])
+    print(app_settings["log_file"])
+    
